6db6ea6d7f2f09d38659f5317d4814dba9cef573
Juan Arean <juan.arean@gmail.com>
2022-08-05 23:03:53 -0300

    interfaces 1

---
+++ interfaces/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+type bot interface {
+	getGreeting() string
+}
+
+type englishBot struct{}
+type spanishBot struct{}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+func (eb englishBot) getGreeting() string {
+	return "Hello"
+}
+
+func (sb spanishBot) getGreeting() string {
+	return "Hola"
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+// func printGreeting(sb spanishBot) {
+// 	fmt.Println(sb.getGreeting())
+// }
+
+// func printGreeting(eb englishBot) {
+// 	fmt.Println(eb.getGreeting())
+// }
